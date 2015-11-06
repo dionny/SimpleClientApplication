0d0f8c706ab093faa90343d723a84566e9e65af4
Dionny Santiago <dionny_santiago@ultimatesoftware.com>
2015-11-06 18:04:40 -0500

    Initial commit.

---
+++ playground.js
@@ -0,0 +1,14 @@
+﻿angular.module('playground', []);
+
+angular.module('playground')
+    .controller('MainCtrl', function ($rootScope, $scope, $http) {
+      $scope.products = [];
+
+      var productId = 1;
+
+      $scope.addProduct = function () {
+          if ($scope.newProductName) {
+              $scope.products.push({ id: productId++, name: $scope.newProductName });
+          }
+      };
+    });
